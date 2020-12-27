5617be8aa38111f5a7fdb715a7e05d7540f72517
Valerio Ageno <valerioageno@yahoo.it>
2020-12-27 19:10:39 +0100

    set fetching API function

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,41 @@
 mod utils;
-
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[derive(Debug, Serialize, Deserialize)]
+pub struct IP {
+    pub ip: String
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, get-ip!");
+pub async fn fetch() ->Result<JsValue, JsValue> {
+
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = format!("https://api.ipify.org?format=json");
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    let window = web_sys::window().unwrap();
+
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    assert!(resp_value.is_instance_of::<Response>());
+
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json()?).await?;
+
+    // Use serde to parse the JSON into a struct.
+    let ip: IP = json.into_serde().unwrap();
+
+    // Send the `Branch` struct back to JS as an `Object`.
+    Ok(JsValue::from_serde(&ip).unwrap())
+    
 }
